b94fd0c7dfd4270dc33303b73ff6dc660e739793
amardeep <amardeep@adoreinfotech.com>
2018-11-13 18:46:07 +0530

    Add menu and fixed onClickListener and OnLongClickListener

--- app/src/main/java/com/contdemo/contdemo/ContactActivity.java
+++ app/src/main/java/com/contdemo/contdemo/ContactActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,6 +20,7 @@ public class ContactActivity extends AppCompatActivity {
     private RecyclerView contactRecyclerView;
     private List<ContactItem> contactItemList;
     private ContactAdapter adapter;
+    private List<String> addedNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,18 @@ public class ContactActivity extends AppCompatActivity {
         contactRecyclerView = findViewById(R.id.contact_recycler);
         getAllContact();
 
-        adapter = new ContactAdapter(contactItemList, getApplicationContext());
+        //adapter = new ContactAdapter(contactItemList, listener, getApplicationContext());
+        adapter = new ContactAdapter(contactItemList, getApplicationContext(), new ContactAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(ContactItem item) {
+                Toast.makeText(getApplicationContext(),item.getContactNumber(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(ContactItem item) {
+                Toast.makeText(getApplicationContext(),item.getContactName(), Toast.LENGTH_SHORT).show();
+            }
+        });
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         contactRecyclerView.setLayoutManager(mLayoutManager);
         contactRecyclerView.setItemAnimator(new DefaultItemAnimator());
@@ -45,5 +59,25 @@ public class ContactActivity extends AppCompatActivity {
             contactItemList.add(item);
         }
         phones.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_phone_number_menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.add_all_number);
+        if (true) {
+            menuItem.setVisible(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.add_all_number) {
+           Toast.makeText(getApplicationContext(), "Check", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/contdemo/contdemo/ContactAdapter.java
+++ app/src/main/java/com/contdemo/contdemo/ContactAdapter.java
@@ -14,17 +14,18 @@ import java.util.List;
 
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
 
-//    public interface OnItemClickListener {
-//        //void onItemClick(ContactItem item);
-//        void onItemLongClick (ContactItem item);
-//    }
+    public interface OnItemClickListener {
+        void onItemClick(ContactItem item);
+        void onItemLongClick (ContactItem item);
+    }
 
     private List<ContactItem> items;
-  //  private final OnItemClickListener listener;
+    private final OnItemClickListener listener;
     private Context context;
 
-    public ContactAdapter(List<ContactItem> items, Context context) {
+    public ContactAdapter(List<ContactItem> items, Context context, OnItemClickListener listener) {
         this.items = items;
+        this.listener = listener;
         this.context = context;
     }
 
@@ -38,18 +39,18 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
-       // viewHolder.bind(items.get(position), listener);
+        viewHolder.bind(items.get(position), listener);
 
-        viewHolder.contactName.setText(items.get(position).getContactName());
-        viewHolder.contactNumber.setText(items.get(position).getContactNumber());
-        viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                ContactItem contactItem = items.get(position);
-                Toast.makeText(context, contactItem.getContactName(), Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
+//        viewHolder.contactName.setText(items.get(position).getContactName());
+//        viewHolder.contactNumber.setText(items.get(position).getContactNumber());
+//        viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View view) {
+//                ContactItem contactItem = items.get(position);
+//                Toast.makeText(context, contactItem.getContactName(), Toast.LENGTH_SHORT).show();
+//                return false;
+//            }
+//        });
     }
 
     @Override
@@ -68,15 +69,23 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
 
         }
 
-//        public void bind(final ContactItem item, final onItemLongClick listener) {
-//            contactName.setText(item.getContactName());
-//            contactNumber.setText(item.getContactNumber());
-//          //  Picasso.with(itemView.getContext()).load(item.imageUrl).into(image);
-//            itemView.setOnClickListener(new View.OnClickListener() {
-//                @Override public void onClick(View v) {
-//                    listener.onItemClick(item);
-//                }
-//            });
-//        }
+        public void bind(final ContactItem item, final OnItemClickListener listener) {
+            contactName.setText(item.getContactName());
+            contactNumber.setText(item.getContactNumber());
+          //  Picasso.with(itemView.getContext()).load(item.imageUrl).into(image);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(item);
+                }
+            });
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    listener.onItemLongClick(item);
+                    return true;
+                }
+            });
+        }
     }
 }
